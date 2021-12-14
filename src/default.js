50b9a89fc2f55882c01e6c5a4b322be99b65ed5d
akosgarai <szlem01@gmail.com>
2021-12-14 20:39:51 +0100

    Move application to module

---
+++ src/default.js
@@ -0,0 +1,34 @@
+import {Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh} from 'three';
+
+class DefaultScreen {
+    constructor() {}
+    run() {
+        this.scene = new Scene();
+        this.camera = new PerspectiveCamera(
+            75, window.innerWidth / window.innerHeight, 0.1, 1000
+        );
+        this.renderer = new WebGLRenderer();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+        const geometry = new BoxGeometry(1, 1, 1);
+        const material = new MeshBasicMaterial({ color: 0x00ff00 });
+        this.cube = new Mesh(geometry, material);
+        this.scene.add(this.cube);
+
+        this.camera.position.z = 5;
+
+        this.animate();
+    }
+    animate() {
+        requestAnimationFrame(this.animate.bind(this));
+        this.render();
+    }
+    render() {
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
+
+        this.renderer.render(this.scene, this.camera);
+    }
+}
+
+export { DefaultScreen };
