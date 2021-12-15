115c5cd78079857243e7499f26ac373c45e2e68a
akosgarai <szlem01@gmail.com>
2021-12-15 20:11:03 +0100

    Refactor default application, extend it from basic screen

--- src/screen/Default.js
+++ src/screen/Default.js
@@ -1,40 +1,31 @@
 import {Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh} from 'three';
+import {BasicScreen} from './BasicScreen.js';
 
-class DefaultScreen {
-    constructor(screen) {
-        this.screenNode = screen;
-    }
-    applicationName() {
-        return 'Default application';
-    }
+class DefaultScreen extends BasicScreen {
     run() {
         this.scene = new Scene();
         this.camera = new PerspectiveCamera(
             75, window.innerWidth / window.innerHeight, 0.1, 1000
         );
         this.renderer = new WebGLRenderer();
+
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.screenNode.appendChild(this.renderer.domElement);
         const geometry = new BoxGeometry(1, 1, 1);
         const material = new MeshBasicMaterial({ color: 0x00ff00 });
-        this.cube = new Mesh(geometry, material);
-        this.scene.add(this.cube);
+        const cube = new Mesh(geometry, material);
+        cube.name = 'cube';
+        this.scene.add(cube);
 
         this.camera.position.z = 5;
 
         this.animate();
     }
-    stop() {
-    }
-    animate() {
-        requestAnimationFrame(this.animate.bind(this));
-        this.render();
-    }
     render() {
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
+        this.scene.getObjectByName('cube').rotation.x += 0.01;
+        this.scene.getObjectByName('cube').rotation.y += 0.01;
 
-        this.renderer.render(this.scene, this.camera);
+        super.render();
     }
 }
 
