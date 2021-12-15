2f2f06271b8417a52defe70cf9ca270bb87e0a35
akosgarai <szlem01@gmail.com>
2021-12-15 20:56:47 +0100

    Refactor gui application, extend it from basic screen

--- src/screen/Gui.js
+++ src/screen/Gui.js
@@ -1,14 +1,14 @@
 import {Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshNormalMaterial, Mesh} from 'three';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
 import GUI from 'lil-gui';
+import {BasicScreen} from './BasicScreen.js';
 
-class GuiScreen {
-    constructor(screen) {
-        this.screenNode = screen;
-        this.control = new function() {
+class GuiScreen extends BasicScreen {
+    constructor(name, screen) {
+        const control = new function() {
             this.rotationSpeed = 0.005;
             this.scale = 1;
         };
+        super(name, screen, control);
     }
     applicationName() {
         return 'Gui screen';
@@ -36,35 +36,20 @@ class GuiScreen {
         this.camera.lookAt(this.scene.position);
 
         // stats
-        this.buildStats();
+        this.buildFPSStats();
 
         // GUI
         this.gui = new GUI();
-        this.gui.add(this.control, 'rotationSpeed', -0.1, 0.1);
-        this.gui.add(this.control, 'scale', 0.01, 2);
+        this.gui.add(this.controls, 'rotationSpeed', -0.1, 0.1);
+        this.gui.add(this.controls, 'scale', 0.01, 2);
 
-        this.animate();
-    }
-    stop() {
-        this.gui.destroy();
-    }
-    animate() {
-        requestAnimationFrame(this.animate.bind(this));
-        this.render();
+        super.run();
     }
     render() {
-        this.renderer.render(this.scene, this.camera);
-        this.stats.update();
-        this.scene.getObjectByName('cube').rotation.x += this.control.rotationSpeed;
-        this.scene.getObjectByName('cube').scale.set(this.control.scale, this.control.scale, this.control.scale);
-    }
-    buildStats() {
-        this.stats = new Stats();
-        this.stats.setMode(0);
-        this.stats.domElement.style.position = 'absolute';
-        this.stats.domElement.style.left = '0';
-        this.stats.domElement.style.top = '20px';
-        this.screenNode.appendChild(this.stats.domElement);
+        this.scene.getObjectByName('cube').rotation.x += this.controls.rotationSpeed;
+        this.scene.getObjectByName('cube').scale.set(this.controls.scale, this.controls.scale, this.controls.scale);
+
+        super.render();
     }
 }
 
