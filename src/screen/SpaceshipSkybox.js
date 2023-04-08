1b587d421198d0dd3bf7b6c4d5be4818237691a9
akosgarai <szlem01@gmail.com>
2023-04-08 12:56:14 +0200

    Spaceship rotation implementation

--- src/screen/SpaceshipSkybox.js
+++ src/screen/SpaceshipSkybox.js
@@ -14,7 +14,10 @@ import {SkyBox} from '../meshes/SkyBox.js';
 // Based on the following document: https://codinhood.com/post/create-skybox-with-threejs
 class SpaceshipSkyboxScreen extends BasicScreen {
     constructor(name, screen) {
-        const control = new function() {};
+        const control = new function() {
+            // rotation of the spaceship around the z axis.
+            this.spaceshipRotation = 0;
+        };
         super(name, screen, control);
     }
     run(gui) {
@@ -35,9 +38,13 @@ class SpaceshipSkyboxScreen extends BasicScreen {
         this.scene.add(ambientLight);
         this.initSpaceShip();
 
+        gui.add(this.controls, 'spaceshipRotation');
+        window.addEventListener('keydown', this.onKeyPress.bind(this), false);
+
         super.run(gui);
     }
     render() {
+        this.updateGui();
         super.render();
     }
     initSpaceShip() {
@@ -48,10 +55,47 @@ class SpaceshipSkyboxScreen extends BasicScreen {
         const texture = new TextureLoader().load('assets/texture/SpaceTruckTop.png');
         const material = new MeshBasicMaterial({ map: texture });
         const spaceship = new Mesh(square, material);
+        spaceship.name = 'spaceship';
         this.scene.add(spaceship);
         // The camera is 100 units above the spaceship.
         this.camera.position.set(0, 0, 1000);
         this.camera.lookAt(spaceship.position);
+    }
+    // Key press event handler.
+    onKeyPress(event) {
+        const ship = this.scene.getObjectByName('spaceship');
+        const code = event.code;
+        switch (code) {
+            case 'ArrowLeft':
+            case 'KeyA':
+                ship.rotation.z += 0.01;
+                break;
+            case 'ArrowRight':
+            case 'KeyD':
+                ship.rotation.z -= 0.01;
+                break;
+        }
+        // The rotation should be between -180 and 180 degrees.
+        if (ship.rotation.z > Math.PI) {
+            ship.rotation.z -= 2 * Math.PI;
+        }
+        if (ship.rotation.z < -Math.PI) {
+            ship.rotation.z += 2 * Math.PI;
+        }
+    }
+    // update the gui. Set the rotation of the spaceship.
+    updateGui() {
+        const ship = this.scene.getObjectByName('spaceship');
+        this.gui.controllers.forEach((item, index) => {
+            switch (item._name) {
+                case 'spaceshipRotation':
+                    // calculate the rotation in degrees.
+                    // right is 90 degrees, left is -90 degrees.
+                    const rotation = ship.rotation.z * -180 / Math.PI;
+                    item.setValue(rotation);
+                    break;
+            }
+        })
     }
 }
 
